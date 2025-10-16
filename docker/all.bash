6505ba20ff79754f8070577ed4c0d4c92bae7d12
Gerald Baulig <gerald.baulig@n-fuse.co>
2025-10-16 16:03:37 +0200

    refactor(envs): add envs and inject envs from a local .env file

--- docker/all.bash
+++ docker/all.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 export NODE_ENV=production
+export RESTART=always
+source .env
 docker compose -f backingservices-docker-compose.yml -f restoreservices-docker-compose.yml $@
--- docker/backing.bash
+++ docker/backing.bash
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+export RESTART=always
+source .env
 docker compose -f backingservices-docker-compose.yml $@

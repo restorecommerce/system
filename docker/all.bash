6cf69da4ff53203238688ef67a03b2f4a1834345
Gerald Baulig <gerald.baulig@n-fuse.co>
2025-10-29 14:37:16 +0100

    fix(all.bash): export compose vars static

--- docker/all.bash
+++ docker/all.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source .default.env
+export ENV_FILE='.default.env'
+export NODE_ENV='production'
+export RESTART='always'
 source .env
 docker compose -f backingservices-docker-compose.yml -f restoreservices-docker-compose.yml $@

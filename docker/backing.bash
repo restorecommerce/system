06735c8b730ab3648b45544265b0eef8fbbec1fe
Gerald Baulig <gerald.baulig@n-fuse.co>
2026-05-18 15:27:57 +0200

    fix(backing): add missing export vars

--- docker/backing.bash
+++ docker/backing.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export ENV_FILE='.default.env'
+export NODE_ENV='production'
 export RESTART=${RESTART:-always}
 source .env
 docker compose -f restoreservices-docker-compose.yml --profile backing $@

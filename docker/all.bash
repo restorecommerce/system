1fd7bc6a85194f86a2af1731c89c01f5ca691869
Gerald Baulig <gerald.baulig@n-fuse.co>
2026-06-03 16:57:31 +0200

    fix(kafka): reduce failing retails but rather restart entire service

--- docker/all.bash
+++ docker/all.bash
@@ -2,6 +2,6 @@
 
 export ENV_FILE='.default.env'
 export NODE_ENV='production'
-export RESTART=${RESTART:-always}
+export RESTART=${RESTART:-unless-stopped}
 source .env
 docker compose -f restoreservices-docker-compose.yml $@
